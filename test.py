ffebb7793d23ecf6af8228f1822f6cbc533d3d60
binbin <yigehenqiangderen@163.com>
2023-11-09 14:23:36 +0800

    1

--- test.py
+++ test.py
@@ -1,25 +1,61 @@
-import bluetooth
+import serial
 
-# 要连接的蓝牙设备的MAC地址
-device_address = '48:E7:29:B6:76:7A'  # 用您要连接的设备的实际地址替换
+port = '/dev/ttyUSB0'
+baud_rate = 115200
+
+ser = serial.Serial(port, baud_rate, timeout=1)
+
+def read_data_packet():
+    # Read until two consecutive bytes 0x55 and 0x61 are found
+    start_bytes = ser.read(1)
+    while start_bytes != b'\x55':
+        start_bytes = ser.read(1)
+
+    # Check for the next byte to be 0x61
+    if ser.read(1) == b'\x61':
+        # Read 18 more bytes after 0x55 and 0x61
+        data_packet = start_bytes + b'\x61' + ser.read(18)
+
+        return data_packet
+    else:
+        # If not found, restart the search
+        return read_data_packet()
+
+def parse_data_packet(data_packet):
+    # Unpack the data_packet based on the format described in the datasheet
+    axL, axH, ayL, ayH, azL, azH, wxL, wxH, wyL, wyH, wzL, wzH, RollL, RollH, PitchL, PitchH, YawL, YawH = data_packet[2:20]
+
+    # Calculate the acceleration values
+    ax = ((axH << 8) | axL) / 32768 * 16  # Acceleration in g (9.8 m/s^2)
+    ay = ((ayH << 8) | ayL) / 32768 * 16  # Acceleration in g (9.8 m/s^2)
+    az = ((azH << 8) | azL) / 32768 * 16  # Acceleration in g (9.8 m/s^2)
+
+    # Calculate the angular velocity values
+    wx = ((wxH << 8) | wxL) / 32768 * 2000  # Angular velocity in °/s
+    wy = ((wyH << 8) | wyL) / 32768 * 2000  # Angular velocity in °/s
+    wz = ((wzH << 8) | wzL) / 32768 * 2000  # Angular velocity in °/s
+
+    # Calculate the roll, pitch, and yaw angles
+    Roll = ((RollH << 8) | RollL) / 32768 * 180  # Roll angle in °
+    Pitch = ((PitchH << 8) | PitchL) / 32768 * 180  # Pitch angle in °
+    Yaw = ((YawH << 8) | YawL) / 32768 * 180  # Yaw angle in °
+
+    return ax, ay, az, wx, wy, wz, Roll, Pitch, Yaw
 
 try:
-    # 创建一个BluetoothSocket对象
-    sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+    while True:
+        # Read the data packet (19 bytes) from the serial port
+        data_packet = read_data_packet()
 
-    # 连接到设备
-    sock.connect((device_address, 1))  # 第二个参数是通道号，通常为1
+        # Parse the data packet to get the sensor values
+        ax, ay, az, wx, wy, wz, Roll, Pitch, Yaw = parse_data_packet(data_packet)
 
-    # 发送数据
-    while(1):
-        sock.send("Hello, Bluetooth!")
+        # Print the sensor values
+        print(f"Acceleration (g): ({ax}, {ay}, {az}), Angular Velocity (°/s): ({wx}, {wy}, {wz}), Roll (°): {Roll}, Pitch (°): {Pitch}, Yaw (°): {Yaw}")
 
-    # 接收数据
-    data = sock.recv(1024)
-    print("Received:", data)
-
-    # 关闭连接
-    sock.close()
+except KeyboardInterrupt:
+    ser.close()
+    print("Serial connection closed.")
 except Exception as e:
-    print("Error:", str(e))
-
+    print("Error:", e)
+    ser.close()
